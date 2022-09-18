b5ce49dc9cd0c4ff8ea4829d90d3211e337363c1
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2022-09-18 21:06:04 +0300

    task 1 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// підключаємо бібліотеки JS
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// виконуємо розмітку
+const gallery = document.querySelector(".gallery");
+
+
+const markup = galleryItems.map((el) => 
+	`<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`
+).join("");
+ 
+gallery.insertAdjacentHTML('beforeend', markup);
+
+// делегування div.gallery отримання url вел. зображення.
+gallery.addEventListener("click", preventDef);
+
+function preventDef(event) {
+	event.preventDefault();
+	  if (event.target.nodeName !== "IMG") {
+    return;
+	};
+	
+	var lightbox = new SimpleLightbox('.gallery .gallery__item', { captionsData: 'alt', captionDelay: 250, animationSpeed: 250, });
+console.log(lightbox);
+};
+
