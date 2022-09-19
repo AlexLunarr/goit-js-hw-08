69d87d75354291a4f93f5369291a82d0a3493251
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2022-09-19 08:08:01 +0300

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeUpdate', throttle(onPlay, 1000));
+
+function onPlay({ timeUpdate }) {
+	let pause = timeUpdate.seconds;
+	localStorage.setItem('videoplayer-current-time', pause);
+	console.log(pause);
+};
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+	
